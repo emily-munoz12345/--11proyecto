9c5109b6521452c9d13f3c6754e57f4c9128b870
paula888997 <paulaabayona@gmail.com>
2025-07-15 23:49:13 -0500

    footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -4,6 +4,25 @@
             <span class="footer-version">v1.0.0</span>
             <span class="footer-copyright">© <?= date('Y') ?> Nacional Tapizados</span>
         </div>
+        <div class="informacion">
+            <div class="whatsapp">
+                <a href="https://wa.me/57(numero de telefono)?text=Hola%2C%20quiero%20más%20información" 
+                    class="btn btn-success" 
+                    target="_blank">
+                    <i class="fab fa-whatsapp"></i> 
+                </a>
+                <a href="https://instagram.com/(nombre de usuario)" 
+                    class="btn btn-danger" 
+                    target="_blank">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="(aca se pega el link del perfil del usuario)" 
+                    class="btn btn-primary" 
+                    target="_blank">
+                <i class="fab fa-facebook-f"></i>
+                </a>
+            </div>
+        </div>
         
         <div class="footer-right">
             <div class="system-status">
